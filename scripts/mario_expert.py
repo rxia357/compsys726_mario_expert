86634c02c6f5e2ec462be0d8a24bafd79a8d21ab
rxia357 <100566786+rxia357@users.noreply.github.com>
2024-09-05 02:52:52 +1200

    can't jump higher

--- scripts/mario_expert.py
+++ scripts/mario_expert.py
@@ -11,6 +11,7 @@ import logging
 import random
 
 import cv2
+import numpy as np
 from mario_environment import MarioEnvironment
 from pyboy.utils import WindowEvent
 
@@ -29,7 +30,7 @@ class MarioController(MarioEnvironment):
 
     def __init__(
         self,
-        act_freq: int = 30,
+        act_freq: int = 5,
         emulation_speed: int = 1,
         headless: bool = False,
     ) -> None:
@@ -63,7 +64,7 @@ class MarioController(MarioEnvironment):
         self.valid_actions = valid_actions
         self.release_button = release_button
 
-    def run_action(self, action: int) -> None:
+    def run_action(self, actions: list, delays: list) -> None:
         """
         This is a very basic example of how this function could be implemented
 
@@ -71,15 +72,21 @@ class MarioController(MarioEnvironment):
 
         You can change the action type to whatever you want or need just remember the base control of the game is pushing buttons
         """
+        self.act_freq = max(delays)
+        if self.act_freq == 0:
+            self.act_freq = 5
+            
+        for i, action in enumerate(actions):
+            self.pyboy.send_input(self.valid_actions[action])
+            self.valid_action = delays[i]
+            if self.act_freq == 0:
+                self.act_freq = 5
 
-        #Simply toggles the buttons being on or off for a duration of act_freq
-        # self.pyboy.send_input(self.valid_actions[action])
+            for _ in range(self.act_freq):
+                self.pyboy.tick()
 
-        for _ in range(self.act_freq):
-            self.pyboy.tick()
-
-        # self.pyboy.send_input(self.release_button[action])
-
+        for i, action in enumerate(actions):
+            self.pyboy.send_input(self.release_button[action])
 
 class MarioExpert:
     """
@@ -94,7 +101,7 @@ class MarioExpert:
         headless (bool, optional): Whether to run the game in headless mode. Defaults to False.
     """
 
-    def __init__(self, results_path: str, headless=False):
+    def __init__(self, results_path: str, headless=True):
         self.results_path = results_path
 
         self.environment = MarioController(headless=headless)
@@ -103,16 +110,71 @@ class MarioExpert:
 
     def choose_action(self):
         state = self.environment.game_state()
-        frame = self.environment.grab_frame()2
+        frame = self.environment.grab_frame()
         game_area = self.environment.game_area()
         
-        print(state)
-        #print(frame)
-        #print(game_area)
+        # print(state)
+        # print(frame)
+        print(game_area)
 
         # Implement your code here to choose the best action
         # time.sleep(0.1)
         # return random.randint(0, len(self.environment.valid_actions) - 1)
+        
+        DOWN = 0
+        LEFT = 1 # walk left
+        RIGHT = 2 # walk right
+        UP = 3
+        A = 4 # jump
+        B = 5
+        
+        # Initilisation
+        mario = [0,0]
+        delay = 0
+        
+        # Find mario's location
+        mario_positions = np.argwhere(game_area == 1)
+        mario = mario_positions[0]
+        
+        if len(mario) == 0:
+            return 0, 0
+        
+        # if len(mario_positions) == 0:
+        #     output = DOWN
+        
+        # go back a bit so it dosesn't collide on the monster when jumping over a pipe
+        if game_area[mario[0]+1][mario[1]+2] == 14 and game_area[mario[0]+1][mario[1]+7] == 15:
+            output = [LEFT]
+            delay = [10]
+        # Check if there's monster right on top of mario, then run to left
+        elif 15 in game_area[:mario[0] - 1, mario[1]+1]:
+            output = [LEFT]
+            delay = [60] # increase delay
+        # if there is something in front of mario, then jump
+        elif game_area[mario[0]+1][mario[1]+ 2] != 0:
+            output = [A]
+            delay = [15]
+        # Check if there is POT HOLE in front of mario, the gap is less than 3 pixels (1-2 pixels wide)
+        elif game_area[mario[0]+2][mario[1]+1] == 10 and game_area[15][mario[1]+2] == 0 and game_area[15][mario[1]+4] != 0:
+            output = [A]
+            delay = [20]
+        # huge POT HOLE, at least 3 pixels wide accelerate first
+        elif game_area[mario[0]+2][mario[1]+1] == 10 and game_area[15][mario[1]+7] == 0 and game_area[15][mario[1]+9] == 0:
+            output = [RIGHT]
+            delay = [30]
+        # elif game_area[mario[0]+2][mario[1]+1] == 10 and game_area[15][mario[1]+2] == 0 and game_area[15][mario[1]+4] == 0:
+        #     output = [A,RIGHT]
+        #     delay = [30]
+        # check for large monsters, and move back
+        elif game_area[mario[0]-1][mario[1]+3] == 18:
+            output = [LEFT]
+            delay = [40]
+        # normally run right
+        else:
+            output = [RIGHT]
+            delay = [0]
+            
+        return output, delay
 
 
     def step(self):
@@ -123,10 +185,10 @@ class MarioExpert:
         """
 
         # Choose an action - button press or other...
-        action = self.choose_action()
+        actions, delays = self.choose_action()
 
         # Run the action on the environment
-        self.environment.run_action(action)
+        self.environment.run_action(actions, delays)
 
     def play(self):
         """
